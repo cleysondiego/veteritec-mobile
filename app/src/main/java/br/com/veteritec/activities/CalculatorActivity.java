ea617770171c26688959451721c053928d8ec9a1
Cleyson Silva <cleyson.dasilva@dieboldnixdorf.com>
2020-05-10 01:00:20 -0300

    Adicionando a Activity da calculadora.

---
+++ app/src/main/java/br/com/veteritec/activities/CalculatorActivity.java
@@ -0,0 +1,16 @@
+package br.com.veteritec.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.veteritec.R;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+}
