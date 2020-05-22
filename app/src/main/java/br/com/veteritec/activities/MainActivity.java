c522b193ef8c87d26b2c90238cf2189464d3841f
Igor Tricarico Marques <igor_tricarico@hotmail.com>
2020-05-22 03:29:42 -0300

    Desenvolvimento intermediário do menu deslizante e seus fragmentos

--- app/src/main/java/br/com/veteritec/activities/MainActivity.java
+++ app/src/main/java/br/com/veteritec/activities/MainActivity.java
@@ -1,25 +1,50 @@
 package br.com.veteritec.activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import br.com.veteritec.R;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private FirebaseAuth mAuth;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CalculatorFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_calculator);
+        }
+
         FirebaseApp.initializeApp(this);
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
@@ -28,15 +53,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent intent = new Intent(this, LoginActivity.class);
             startActivityForResult(intent, 1);
         }
-
-        Button btnCalculator = findViewById(R.id.button);
-
-        btnCalculator.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View v) {
-        Intent intent = new Intent(this, CalculatorActivity.class);
-        startActivity(intent);
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_calculator:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CalculatorFragment()).commit();
+                break;
+            case R.id.nav_add_vaccine:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AddVaccineFragment()).commit();
+                break;
+            case R.id.nav_query_vaccine:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new QueryVaccineFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
