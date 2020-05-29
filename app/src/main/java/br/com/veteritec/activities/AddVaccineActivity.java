ae8d82754abd1e055585e9639b3c2acf2f23700c
Cleyson Silva <cleyson.dasilva@dieboldnixdorf.com>
2020-05-28 22:37:07 -0300

    Salvando as informações no FirebaseFirestore.

--- app/src/main/java/br/com/veteritec/activities/AddVaccineActivity.java
+++ app/src/main/java/br/com/veteritec/activities/AddVaccineActivity.java
@@ -12,35 +12,52 @@ import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TimePicker;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import br.com.veteritec.R;
 
 public class AddVaccineActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
+    Spinner spnVeterinary;
+    Spinner spnClient;
+
     EditText edtDate;
     EditText edtTime;
+    EditText edtDescription;
 
     Button btnTime;
     Button btnDate;
+    Button btnSave;
+
+    private FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_vaccine);
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.txtAddVaccineTitle);
@@ -54,14 +71,23 @@ public class AddVaccineActivity extends AppCompatActivity implements View.OnClic
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        spnVeterinary = findViewById(R.id.spnVeterinary);
+        spnClient = findViewById(R.id.spnClient);
+
         edtDate = findViewById(R.id.edtDate);
         edtTime = findViewById(R.id.edtTime);
+        edtDescription = findViewById(R.id.edtDescription);
 
         edtDate.setInputType(InputType.TYPE_NULL);
         edtTime.setInputType(InputType.TYPE_NULL);
 
         btnTime = findViewById(R.id.btnTime);
         btnDate = findViewById(R.id.btnDate);
+        btnSave = findViewById(R.id.btnSave);
+
+        btnTime.setOnClickListener(this);
+        btnDate.setOnClickListener(this);
+        btnSave.setOnClickListener(this);
     }
 
     @Override
@@ -71,6 +97,43 @@ public class AddVaccineActivity extends AppCompatActivity implements View.OnClic
             showDateDialog(edtDate);
         } else if (id == R.id.btnTime) {
             showTimeDialog(edtTime);
+        } else if (id == R.id.btnSave) {
+
+            saveVaccine();
+            Log.e("VETERITEC", "CHAMOU O SAVE VACINE");
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_calculator:
+                Intent calculator = new Intent(this, CalculatorActivity.class);
+                startActivity(calculator);
+                finish();
+                break;
+            case R.id.nav_add_vaccine:
+                break;
+            case R.id.nav_query_vaccine:
+                Intent queryVaccine = new Intent(this, QueryVaccineActivity.class);
+                startActivity(queryVaccine);
+                finish();
+                break;
+            case R.id.nav_logout:
+                FirebaseAuth.getInstance().signOut();
+                finish();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 
@@ -108,36 +171,36 @@ public class AddVaccineActivity extends AppCompatActivity implements View.OnClic
         new DatePickerDialog(AddVaccineActivity.this, dateSetListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.nav_calculator:
-                Intent calculator = new Intent(this, CalculatorActivity.class);
-                startActivity(calculator);
-                finish();
-                break;
-            case R.id.nav_add_vaccine:
-                break;
-            case R.id.nav_query_vaccine:
-                Intent queryVaccine = new Intent(this, QueryVaccineActivity.class);
-                startActivity(queryVaccine);
-                finish();
-                break;
-            case R.id.nav_logout:
-                FirebaseAuth.getInstance().signOut();
-                finish();
-                break;
-        }
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
+    private void saveVaccine() {
+        String veterinary = ""; // spnVeterinary.getSelectedItem().toString();
+        String client = ""; // spnClient.getSelectedItem().toString();
 
-    @Override
-    public void onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+        String edtDate = this.edtDate.getText().toString();
+        String edtTime = this.edtTime.getText().toString();
+        String edtDescription = this.edtDescription.getText().toString();
+
+        Map<String, String> newVaccine = new HashMap<>();
+        newVaccine.put("veterinary", veterinary);
+        newVaccine.put("client", client);
+        newVaccine.put("date", edtDate);
+        newVaccine.put("time", edtTime);
+        newVaccine.put("description", edtDescription);
+
+        firebaseFirestore
+                .collection("customer")
+                .document("bichodamata")
+                .collection("vaccines")
+                .add(newVaccine)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Log.d("VETERITEC", "Documento adicionado com sucesso: " + documentReference.getPath());
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w("VETERITEC", "Campo falhou com a excessão: " + e);
+            }
+        });
     }
 }
