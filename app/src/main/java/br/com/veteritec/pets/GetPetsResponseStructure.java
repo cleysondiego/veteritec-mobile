e7109413bcf524b6f06613a6ae07d17d1c2ee155
Cleyson Silva <cleyson.dasilva@dieboldnixdorf.com>
2020-06-25 21:01:14 -0300

    Realizando a chamada para obter a lista de pets cadastrados.

---
+++ app/src/main/java/br/com/veteritec/pets/GetPetsResponseStructure.java
@@ -0,0 +1,98 @@
+package br.com.veteritec.pets;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GetPetsResponseStructure {
+    private List<Pet> pets = new ArrayList<>();
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public GetPetsResponseStructure fromJson(JSONObject jsonObject) {
+        try {
+            JSONArray jsonArray = jsonObject.getJSONArray("pets");
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject object = jsonArray.getJSONObject(i);
+                pets.add(new Pet().fromJson(object));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return this;
+    }
+
+    public static class Pet implements Serializable {
+        private String id;
+        private String name;
+        private String birth;
+        private String species;
+        private String breed;
+        private String weight;
+        private String comments;
+        private String customer;
+        private String clinic;
+
+        public String getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getBirth() {
+            return birth;
+        }
+
+        public String getSpecies() {
+            return species;
+        }
+
+        public String getBreed() {
+            return breed;
+        }
+
+        public String getWeight() {
+            return weight;
+        }
+
+        public String getComments() {
+            return comments;
+        }
+
+        public String getCustomer() {
+            return customer;
+        }
+
+        public String getClinic() {
+            return clinic;
+        }
+
+        public Pet fromJson(JSONObject jsonObject) {
+            try {
+                this.id = jsonObject.getString("_id");
+                this.name = jsonObject.getString("name");
+                this.birth = jsonObject.getString("birth");
+                this.species = jsonObject.getString("species");
+                this.breed = jsonObject.getString("breed");
+                this.weight = jsonObject.getString("weight");
+                this.comments = jsonObject.getString("comments");
+                this.customer = jsonObject.getString("customer");
+                this.clinic = jsonObject.getString("clinic");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return this;
+        }
+    }
+
+
+}
